6bcd9140a429ad30152ba3e84ec2c41910e1db4c
unknown <akhilkashyap16@gmail.com>
2023-10-23 00:07:04 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const targetDate = new Date("December 12, 2023 19:50:59").getTime(); // Change this to your target date and time
+let countdownInterval;
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = String(days).padStart(2, '0');
+    document.getElementById("hours").textContent = String(hours).padStart(2, '0');
+    document.getElementById("minutes").textContent = String(minutes).padStart(2, '0');
+    document.getElementById("seconds").textContent = String(seconds).padStart(2, '0');
+
+    if (distance < 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("countdown").innerHTML = "The event has already occurred!";
+    }
+}
+
+countdownInterval = setInterval(updateCountdown, 1000); // Update every second
+updateCountdown(); // Initial display
